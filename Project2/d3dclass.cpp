5bdd08613d0a2b70a9750adc08a5d91105b1c075
JD <jepdraper@gmail.com>
2019-10-10 13:50:57 +0100

    Continued work on implementing DirectX

--- Project2/d3dclass.cpp
+++ Project2/d3dclass.cpp
@@ -125,6 +125,7 @@ bool D3DClass::Initialize(int screen_width, int screen_height, bool vsync,
 
 	swap_chain_desc.OutputWindow = hwnd;
 
+	//Multi Sampling OFF
 	swap_chain_desc.SampleDesc.Count = 1;
 	swap_chain_desc.SampleDesc.Quality = 0;
 
@@ -146,11 +147,92 @@ bool D3DClass::Initialize(int screen_width, int screen_height, bool vsync,
 
 	feature_level = D3D_FEATURE_LEVEL_11_0;
 
+	//Swap Chain & DirectX Device
 	result = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE,
-				NULL, 0, &feature_level, 1, D3D11_SDK_VERSION, &swap_chain_desc,
-								&m_swapChain, &m_device, nullptr, &m_deviceContext);
-	if (FAILED(result))
-	{
-		return false;
-	}
+		NULL, 0, &feature_level, 1, D3D11_SDK_VERSION, &swap_chain_desc,
+		&m_swapChain, &m_device, nullptr, &m_deviceContext);
+	if (FAILED(result)) { return false; }
+
+	//Back Buffer Pointer
+	result = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& back_buffer_ptr);
+	if (FAILED(result)) { return false; }
+
+	result = m_device->CreateRenderTargetView(back_buffer_ptr, nullptr, &m_renderTargetView);
+	if (FAILED(result)) { return false; }
+
+	back_buffer_ptr->Release();
+	back_buffer_ptr = nulptr;
+
+	//Depth Buffer
+	ZeroMemory(&depth_buffer_desc, sizeof(depth_buffer_desc));
+
+	depth_buffer_desc.Width = screen_width;
+	depth_buffer_desc.Height = screen_height;
+	depth_buffer_desc.MipLevels = 1;
+	depth_buffer_desc.ArraySize = 1;
+	depth_buffer_desc.format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depth_buffer_desc.SampleDesc.Count = 1;
+	depth_buffer_desc.SampleDesc.Quality = 0;
+	depth_buffer_desc.Usage = D3D11_USAGE_DEFAULT;
+	depth_buffer_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depth_buffer_desc.CPUAccessFlags = 0;
+	depth_buffer_desc.MiscFlags = 0;
+
+	result = m_device->CreateTexture2D(&depth_buffer_desc, nullptr, &m_depthStencilBuffer);
+	if (FAILED(result)) { return false; }
+
+	//Depth Stencil State
+	ZeroMemory(&depth_stencil_desc, sizeof(depth_stencil_desc));
+
+	depth_stencil_desc.DepthEnable = true;
+	depth_stencil_desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	depth_stencil_desc.DepthFunc = D3D11_COMPARISON_LESS;
+
+	depth_stencil_desc.StencilEnable = true;
+	depth_stencil_desc.StencilReadMask = 0xFF;
+	depth_stencil_desc.StencilWriteMask = 0xFF;
+
+	depth_stencil_desc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	depth_stencil_desc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	depth_stencil_desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depth_stencil_desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	depth_stencil_desc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	depth_stencil_desc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	depth_stencil_desc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depth_stencil_desc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	result = m_device->CreateDepthStencilState(&depth_stencil_desc, &m_depthStencilState);
+	if (FAILED(result)) { return false; }
+
+	m_deviceContex->OMSetDepthStencilState(m_depthStencilState, 1);
+
+	//Depth Stencil View
+	ZeroMemory(&depth_stencil_desc, sizeof(depth_stencil_view_desc));
+
+	depth_stencil_view_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depth_stencil_view_desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depth_stencil_view_desc.Texture2D.MipSlice = 0;
+
+	result = m_device->CreateDepthStencilView(m_depthStencilBuffer, &depth_stencil_view_desc, &m_depthStencilView);
+	if (FAILED(result)) { return false; }
+
+	m_deviceContext->OMSetRenderTargets(1, &m_renderTargetView, m_depthStencilView);
+
+	//Raster Description
+	raster_desc.AntialiasedLineEnable = false;
+	raster_desc.CullMode = D3D11_CULL_BACK;
+	raster_desc.DepthBias = 0;
+	raster_desc.DepthBiasClamp = 0.0f;
+	raster_desc.DepthClipEnable = true;
+	raster_desc.FillMode = D3D11_FILL_SOLID;
+	raster_desc.FrontCounterClockwise = false;
+	raster_desc.MultisampleEnable = false;
+	raster_desc.ScissorEnable = false;
+	raster_desc.SlopeScaledDepthBias = 0.0f;
+
+	result = m_device->CreateRasterizerState(&raster_desc, &m_rasterState);
+	if (FAILED(result)) { return false; }
+
+	m_deviceContext->RSSetState(m_rasterState);
 }
